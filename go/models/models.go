1e3cbc4f31ce02b9f98ef1b05cd4a63f7c91bad7
SEDocotor <158790696+SEDocotor@users.noreply.github.com>
2025-12-09 22:55:13 -0800

    Update models.go

--- go/models/models.go
+++ go/models/models.go
@@ -2,7 +2,6 @@ package models
 
 import "time"
 
-// Channel represents a mapping of server/mode/guild -> discord channel
 type Channel struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Server    string    `gorm:"uniqueIndex:idx_channel,priority:1" json:"server"`
@@ -13,7 +12,6 @@ type Channel struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-// ReportedBattle stores which battles have already been reported per server
 type ReportedBattle struct {
 	ID        uint      `gorm:"primaryKey" json:"id"`
 	Server    string    `gorm:"index:idx_reported,priority:1" json:"server"`
